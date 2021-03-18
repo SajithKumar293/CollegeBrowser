43d392f590094836202cfb5be5cb2198b1c8433c
Sajith Kumar <111701012@smail.iitpkd.ac.in>
2021-03-19 04:55:02 +0530

    Final Deployement prep

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const routes = require('./routes/api')
 mongoose.connection.on('open',() => {
     console.log('Connected to mongodb:', mongoose.connection.host);
 })
-mongoose.connect(process.env.MONGODB_URI,{useNewUrlParser:true , useUnifiedTopology: true,useCreateIndex:true});
+mongoose.connect(process.env.MONGO_URI,{useNewUrlParser:true , useUnifiedTopology: true,useCreateIndex:true});
 
 app.use(bodyparser.json());
 app.use(cors());
